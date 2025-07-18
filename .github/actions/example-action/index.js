0918559dccc95e11c77ba21cba46f4c416494565
PowerEntwickler <github@hyper-net.de>
2025-07-18 09:56:21 +0200

    action

--- .github/actions/example-action/index.js
+++ .github/actions/example-action/index.js
@@ -1,10 +1,12 @@
-import * as core from '@actions/core'
+//const core = require('@actions/core');
 
 try {
-    const input = core.getInput('my-input');
-    console.log(`Hello ${input}!`);
+//    const input = core.getInput('my-input');
+//    console.log(`Hello ${input}!`);
+    console.log(`Hello Input!`);
 
-    const output = `Processed input: ${input}`;
+    //const output = `Processed input: ${input}`;
+    const output = `Processed input: INPUT`;
     core.setOutput('my-output', output);
 } catch (error) {
     core.setFailed(error.message);
